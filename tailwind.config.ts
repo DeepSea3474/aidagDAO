bb2a5b5a507c40f9130d6e1fdd07475682e2b619
DeepSea <deepsea@example.com>
2026-03-15 20:30:24 +0100

    🔱 UI_FIX: Cleaning up PostCSS 'antialiased' error and resetting tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,12 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: ["class"],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        border: "hsl(240 5.9% 90%)",
-        input: "hsl(240 5.9% 90%)",
-        ring: "hsl(240 5.9% 10%)",
-        background: "hsl(0 0% 100%)",
-        foreground: "hsl(240 10% 3.9%)",
-        primary: {
-          DEFAULT: "hsl(240 5.9% 10%)",
-          foreground: "hsl(0 0% 98%)",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 } satisfies Config;
