636055f749f56724a10d7ec61e09941ecf796dfc
DeepSea <deepsea@example.com>
2026-03-15 20:15:38 +0100

    🔱 UI_RESTORE: Force include all source files in build

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
+  server: {
+    host: '0.0.0.0',
+  },
 });
